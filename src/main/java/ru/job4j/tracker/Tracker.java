a2af5d3da353cc2f653e0bce7b0a0e766e306fd2
PavelM2205 <pavelm2205@mail.ru>
2022-02-07 15:02:16 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -27,15 +27,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int index = 0; index < size; index++) {
-            if (items[index] != null) {
-                rsl[count++] = items[index];
-            }
-        }
-        rsl = Arrays.copyOf(rsl, count);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
