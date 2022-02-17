435326b43f6c2d9a245aad01dd3d91c363176de2
PavelM2205 <pavelm2205@mail.ru>
2022-02-17 10:33:12 +0700

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1234@mail.ru", "Ivanov Ivan Ivanovich");
+        map.put("5678@mail.ru", "Petrov Petr Petrovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
