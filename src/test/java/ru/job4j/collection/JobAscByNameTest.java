51fe1a4ca97aac6a50e651a19dc24a7aad6b9c0c
PavelM2205 <pavelm2205@mail.ru>
2022-02-19 20:13:25 +0700

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobAscByNameTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class JobAscByNameTest {
+
+    @Test
+    public void compare() {
+        List<Job> jobs = Arrays.asList(
+                new Job("A", 2),
+                new Job("C", 4),
+                new Job("B", 1)
+        );
+        List<Job> expected = Arrays.asList(
+                new Job("A", 2),
+                new Job("B", 1),
+                new Job("C", 4)
+        );
+        Collections.sort(jobs, new JobAscByName());
+        assertEquals(expected, jobs);
+    }
+}
